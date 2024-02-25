ef7d771a787620e18c98476010dd796b14410bbf
Aqil Contractor <aqilcm@gmail.com>
2024-02-24 19:25:34 -0500

    y
    Co-authored-by: eddy ramirez <eddyramirez@users.noreply.github.com>

--- src/screens/welcome.tsx
+++ src/screens/welcome.tsx
@@ -1,6 +1,7 @@
 import { StyleSheet, View, TextInput, Text, Button, Pressable } from "react-native";
 import GradientBackground from "../components/gradientbackground";
 import GradientThing from "../components/gradientthing";
+import { moderateScale } from "../helpers/fontsize";
 
 
 export default function Welcome({ navigation }) {
@@ -8,8 +9,8 @@ export default function Welcome({ navigation }) {
 		<GradientBackground>
 			<Text style={styles.title}>Welcome</Text>
 			<GradientThing style={styles.thing}>
-				<h2>The new way for Gen Z's to connect on campus.</h2>
-				<p>create an account to join</p>
+				<Text style={styles.h1}>The new way for Gen Z's to connect on campus.</Text>
+				<Text>create an account to join</Text>
 			</GradientThing>
 			<Pressable onPress={() => navigation.navigate('Login')}>
 				<Text>Go to Login</Text>
@@ -27,10 +28,20 @@ const styles = StyleSheet.create({
 	},
 	thing: {
 		width: '70%',
-		height: 500,
+		height: moderateScale(500),
 		fontFamily: 'Grotesk Bold',
 		color: 'white',
 		borderRadius: 20,
+		display: 'flex',
+		alignItems: 'center',
+		justifyContent: 'center',
+		padding: 20,
+		textAlign: 'center',
+	},
+	h1: {
+		fontSize: moderateScale(24),
+		marginBottom: moderateScale(24),
+		color: 'white',
 	},
 	title: {
 		fontSize: 24,
